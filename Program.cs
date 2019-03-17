26e9f4f2d5cb0db766fc201e661dc5bef9c545a0
Doruk Saraloğlu <45631089+Tendrony@users.noreply.github.com>
2019-03-17 22:03:37 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumarable
+{
+    class Test : IEnumerable<int>
+    {
+        public int[] Array { get; set; }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < Array.Length; i++)
+            {
+                yield return Array[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = new Test();
+            t.Array = new int[3];
+            t.Array[0] = 5;
+            t.Array[1] = 8;
+            t.Array[2] = 52;
+
+            foreach (var item in t.Array)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
